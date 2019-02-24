a9bde9f7eb1d2548f00bd960fbfe22dcbf7c38b6
StefanTieber <38786543+StefanTieber@users.noreply.github.com>
2019-02-24 19:51:58 +0100

    random

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,44 @@
 import FileHandling.FileReader;
+import FileHandling.FileWriter;
 import Other.Pizza;
+import Other.Slice;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Main {
+    private static Random random = new Random();
+
     public static void main(String[] args) {
         FileReader.readFile();
 
-        Random random = new Random();
+        Pizza pizza = new Pizza();
+        Pizza temp_pizza = new Pizza();
+        boolean activ = true;
 
-        int randX = random.nextInt();
+        while (activ) {
 
-        Pizza.print();
+            Slice randomSlice = new Slice();
+
+            randomSlice.x1 = random.nextInt(Pizza.X);
+            randomSlice.y1 = random.nextInt(Pizza.Y);
+            randomSlice.x2 = randomSlice.x1 + random.nextInt(Pizza.MAX_SIZE);
+            randomSlice.y2 = randomSlice.y1 + random.nextInt(Pizza.MAX_SIZE);
+
+            if(randomSlice.isValid()) {
+                temp_pizza.slices.add(randomSlice);
+
+                if(temp_pizza.isValid()) {
+                    pizza.slices.add(randomSlice);
+                } else {
+                    activ = false;
+                }
+            } else {
+                activ = false;
+            }
+        }
+
+        FileWriter.writeFile(pizza.slices);
     }
 }
