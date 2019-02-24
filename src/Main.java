fe23ec2fb3f152571a03b9809101536f509f59c7
VanillaEGirl <thomas.tieber@gmail.com>
2019-02-24 19:10:58 +0100

    remove stuff in main

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,10 @@
 import FileHandling.FileReader;
 import Other.Pizza;
 
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        List<String> file = FileReader.readFile();
+        FileReader.readFile();
 
         Pizza.print();
-    }
-
-    public static void printStringList(List<String> lines) {
-        for (String line : lines) {
-            System.out.println(line);
-        }
     }
 }
