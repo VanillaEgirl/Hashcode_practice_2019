89d7f3038604a85edcea842aed10c40e67c2564a
VanillaEGirl <thomas.tieber@gmail.com>
2019-02-24 18:33:36 +0100

    extract filepath from filereading

--- src/FileHandling/FileHandler.java
+++ src/FileHandling/FileHandler.java
@@ -6,16 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileHandler {
-    private static String DIR = "C:\\Users\\Thomas\\Downloads\\";
-    private static String FILE = "test";
-    private static String EXT = ".txt";
-    private static String filePath = DIR + FILE + EXT;
-
     public static List<String> readFile() {
 
         List<String> lines = new ArrayList<String>();
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            BufferedReader reader = new BufferedReader(new FileReader(FilePath.path));
 
             String line = " ";
             while (!line.isEmpty()) {
