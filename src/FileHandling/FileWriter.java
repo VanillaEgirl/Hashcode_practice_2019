dc9b1870461f897849fd3b92916f0e34993f88fa
StefanTieber <38786543+StefanTieber@users.noreply.github.com>
2019-02-24 19:17:51 +0100

    Implement FlieWriter

--- src/FileHandling/FileWriter.java
+++ src/FileHandling/FileWriter.java
@@ -2,8 +2,28 @@ package FileHandling;
 
 import Other.Slice;
 
-public class FileWriter {
-    public static void writeSlice(Slice slice) {
+import java.io.PrintWriter;
+import java.util.List;
 
+public class FileWriter {
+    private static final String ENCODING = "UTF-8";
+
+    public static void writeFile(List<Slice> slices) {
+        String filePath = FilePath.outputPath;
+
+        try {
+            PrintWriter writer = new PrintWriter(filePath, ENCODING);
+
+            for (Slice slice : slices) {
+                String string = slice.y1 + " " + slice.x1 + " " + slice.y2 + " " + slice.x2;
+                System.out.println(string);
+                writer.println(string);
+            }
+
+            writer.close();
+        } catch (Exception e) {
+            System.err.println("Exception occurred trying to write " + filePath);
+            e.printStackTrace();
+        }
     }
 }
